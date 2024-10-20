4ae9af6adcaba834da6dadb0a2b0d9af4146cd5d
Vaisakh2004 <vaisakhramakrishnan@gmail.com>
2024-10-20 12:17:31 +0530

    Update dashboard.js

--- dashboard.js
+++ dashboard.js
@@ -1,13 +1,13 @@
 document.addEventListener('DOMContentLoaded', () => {
-    document.getElementById('go-to-employees').addEventListener('click', () => {
-        window.location.href = 'employees.html';
-    });
-
-    document.getElementById('go-to-occupants').addEventListener('click', () => {
+    document.getElementById('occupantsBtn').addEventListener('click', () => {
         window.location.href = 'occupants.html';
     });
 
-    document.getElementById('go-to-rooms').addEventListener('click', () => {
+    document.getElementById('employeesBtn').addEventListener('click', () => {
+        window.location.href = 'employees.html';
+    });
+
+    document.getElementById('roomsBtn').addEventListener('click', () => {
         window.location.href = 'rooms.html';
     });
 });
